363aed11e9206755b9ed10092e7f80768e51b51c
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-03-25 23:52:58 -0300

    Alterações de layout e traduções

--- app/src/main/java/com/unisc/exaula2_4/MainActivity.java
+++ app/src/main/java/com/unisc/exaula2_4/MainActivity.java
@@ -2,6 +2,8 @@ package com.unisc.exaula2_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#808080")));
+
+        this.setTitle(getResources().getString(R.string.titulo));
     }
 }
